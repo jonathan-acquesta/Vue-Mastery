c2e92a701930f9af9446cc907e0161fc4d3cbb99
Jonathan Caravaggio Acquesta <jonathan.acquesta@hotmail.com>
2020-04-17 21:40:55 -0300

    Vue Mastery Intro to Vue Attribute Binding

---
+++ 1st Intro to Vue/2. Attribute Binding/main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        description: 'A pair of warm, fuzzy socks',
+        link: 'http://www.jonathancacquesta.com.br',
+        image: './assets/vmSocks-green-onWhite.jpg'
+    }
+})
