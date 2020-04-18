e6fc8323145a5fbab2d3b8499edc4efed942e45e
Jonathan Caravaggio Acquesta <jonathan.acquesta@hotmail.com>
2020-04-17 23:34:47 -0300

    Vue Mastery Intro Vue Event Handling

---
+++ 1st Intro to Vue/5. Event Handling/README.md
@@ -0,0 +1,14 @@
+# What’d we Learn
+- The v-on directive is used to allow elements to listen for events
+- The shorthand for v-on is @
+- You can specify the type of event to listen for:
+    - click
+    - mouseover
+    - any other DOM event
+- The v-on directive can trigger a method
+- Triggered methods can take in arguments
+- this refers to the current Vue instance’s data as well as other methods declared inside the instance
+
+# Learn by doing
+## Challenge:
+Create a new button and method to decrement the value of cart.
---
+++ 1st Intro to Vue/5. Event Handling/main.js
@@ -0,0 +1,33 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        inventory: 12,
+        image: './assets/vmSocks-green-onWhite.jpg',
+        onSale: true,
+        cart: 0,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        sizes: ["P", "M", "G"],
+        variants: [
+            { id: 1, color: "green", image: './assets/vmSocks-green-onWhite.jpg' },
+            { id: 2, color: "blue", image: './assets/vmSocks-blue-onWhite.jpg' }
+        ]
+    },
+    methods: {
+        addToCart() {
+            if (this.inventory > 0) {
+                ++this.cart;
+                --this.inventory;
+            }
+        },
+        removeFromCart() {
+            if (this.cart > 0) {
+                --this.cart;
+                ++this.inventory;
+            }
+        },
+        updateProduct(image) {
+            this.image = image;
+        }
+    }
+})
